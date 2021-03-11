9b33982c2156237c70c746613ccc0827d4be285d
bartlomiejkrawczyniuk <Ziomek1995@>
2021-03-11 21:14:52 +0100

    first commit

---
+++ src/test/java/pl/kurs/btccalculator/BtcCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.btccalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
